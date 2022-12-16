49bdb0033df56a9e248da294a8f816edff1a613d
myogin <yoginugraha19@gmail.com>
2022-12-16 08:33:07 +0800

    tes

---
+++ main.js
@@ -0,0 +1,49 @@
+(function () {
+  ("use strict");
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    if (all) {
+      select(el, all).forEach((e) => e.addEventListener(type, listener));
+    } else {
+      select(el, all).addEventListener(type, listener);
+    }
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select("#header");
+
+  const headerScrolled = () => {
+    if (window.scrollY > 100) {
+      selectHeader.classList.add("header-scrolled");
+    } else {
+      selectHeader.classList.remove("header-scrolled");
+    }
+  };
+  window.addEventListener("load", headerScrolled);
+  onscroll(document, headerScrolled);
+
+ 
+})();
